7970a95db14d2b80bcca87331fdf336deca5a807
angelichazu <facquer98@gmail.com>
2021-03-10 15:34:40 -0500

    Creacion header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'personaje-detalle/:id', loadChildren: () => import('./components/pages/personajes/personaje-detalle/personaje-detalle.module').then(m => m.PersonajeDetalleModule) }, { path: 'personaje-list', loadChildren: () => import('./components/pages/personajes/personaje-list/personaje-list.module').then(m => m.PersonajeListModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/pages/personajes/personaje-list/personaje-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personaje-list',
+  templateUrl: './personaje-list.component.html',
+  styleUrls: ['./personaje-list.component.scss']
+})
+export class PersonajeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
